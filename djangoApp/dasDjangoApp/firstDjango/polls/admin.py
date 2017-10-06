1c6368687b5f73f3c2ccaf2b948c9e68a0a7a047
RobertWWong <robertww@uci.edu>
2017-10-05 23:19:52 -0700

    part2/devServer Just fiddling with the dev server, understanding how to display data relevant to the admin. Off for tonight, study more tomorrow.

--- djangoApp/dasDjangoApp/firstDjango/polls/admin.py
+++ djangoApp/dasDjangoApp/firstDjango/polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+# Register your models here. So you can see it on admind site
+
+admin.site.register([Question,Choice])

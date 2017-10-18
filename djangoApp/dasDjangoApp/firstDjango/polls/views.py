785e50a66636dbcab3b2c36c1bc5418081f36ee9
archon263 <archon263@gmail.com>
2017-10-17 22:03:54 -0700

    Django presents another way to display views via generic Class objects. The generic class allows for wrapper functions to operate as RESTful interfaces. It is also uses multi-inheir concept (mixin) in its other usage (i'll look it up)

--- djangoApp/dasDjangoApp/firstDjango/polls/views.py
+++ djangoApp/dasDjangoApp/firstDjango/polls/views.py
@@ -1,54 +1,30 @@
 from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponseRedirect  
+from django.urls import reverse
+from django.views import generic
 
-from django.http import HttpResponse, HttpResponseRedirect  	#Dont need this if we use render()
-from django.http import Http404
-
-from django.template import loader 		#Dont need this if we use render()
 
 from .models import Question, Choice
-from django.urls import reverse
 # Create your views here. You will be assigning url regex via urls.py of your resepect view directory
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def index(request):
-	# print("Yes, do treat your HttpResponses as html iterpreter.")
-
-	#We don't want to hard-code our views. Wouldn't be OOP.
-	latest_q_list = Question.objects.order_by('-pub_date')[:5]
-
-	# Here's one way to render your html files, but it's a bit klunky
-	# template = loader.get_template('polls/index.html')
-	# context = {
-	# 'latest_q_list' : latest_q_list
-	# }
-	context ={				#Think of your context as the data-bound value,  or ng-model
-		'latest_q_list': latest_q_list
-	}
-	yourTemplatePath = 'polls/index.html'	#Since we have set up our template folder, 
-	#and we won't need to import loader and HttpResponse if we use Render like this
-
-	return render(request,  yourTemplatePath, context)
+	def get_queryset(self):
+		'''Return the last five published questions.'''
+		return Question.objects.order_by('-pub_date')[:5]
 
 
-
-#Add some more views to our polls
-def detail( request, q_id):
-	"""
-	Time to write some 404 cases:
-	Here's a hint, it's a helper function for django.shortcuts get_object_or_404
-	"""
-	question = get_object_or_404(Question, pk=q_id)
-	# try:
-	# 	question = Question.objects.get(pk= question_id)
-	# except Question.DoesNotExist:
-	# 	raise Http404("Yeah, that's not a question at all. Well, around here that is.")
-	return render(request, 'polls/detail.html', {"question": question})
+#Now our views will be an object itself
+class DetailView (generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+	
 
 
-def results(request, q_id):
-	# response = "You're looking at question %s." 
-	# return HttpResponse(response % q_id)		#Use a str var to later format it
-	quest = get_object_or_404(Question, pk=q_id)
-	return render ( request, 'polls/results.html', {'question': quest})
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, q_id):
 	quest = get_object_or_404(Question, pk = q_id)
